cdb3525660d1b4bcd59cad0583c4299d0a8c95d8
Ramya-Shah <176913971+Ramya-Clg@users.noreply.github.com>
2025-05-06 21:00:06 +0530

    change in mdes

--- src/app/mdes/HeroSection.tsx
+++ src/app/mdes/HeroSection.tsx
@@ -103,82 +103,36 @@ const HeroSection = () => {
           </motion.div>
         </div>
 
-        {/* Animated grid with statistic cards and logo */}
-        <motion.div
-          className="grid grid-cols-1 gap-8"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true, amount: 0.5 }}
-          variants={{
-            visible: {
-              opacity: 1,
-              y: 0,
-              transition: { staggerChildren: 0.1, duration: 0.5 },
-            },
-            hidden: { opacity: 0, y: 20 },
-          }}
-        >
-          <h2 className="text-2xl font-semibold mb-16 mx-auto">
-            Programs offered under{" "}
-            <span className="bg-gradient-to-r from-[#EF4023] to-[#FCBB4D] text-white px-4 py-1 rounded-lg">
-              M.Des
-            </span>
-          </h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-10 w-full">
-            {[
-              {
-                title: "6 LPA",
-                subtitle: "Median Salary",
-                bg: "bg-white",
-                text: "text-blue-900",
-              },
-              {
-                title: "36.44 LPA",
-                subtitle: "Highest Placement",
-                bg: "bg-red-600",
-                text: "text-white",
-              },
-              {
-                title: "Intelligent UX Design",
-                subtitle: "By Govt. of Gujarat",
-                bg: "bg-white",
-                text: "text-blue-900",
-              },
-              {
-                title: "Communication Design",
-                subtitle: "By GSIRF",
-                bg: "bg-white",
-                text: "text-blue-900",
-              },
-            ]
-              .slice(2, 4)
-              .map((item, index) => (
-                <motion.div
-                  key={index}
-                  whileHover={{ scale: 1.05, transition: { duration: 0 } }}
-                  className={`p-6 shadow-md rounded-md transition transform duration-300 hover:shadow-lg bg-gray-100 hover:bg-red-600 group col-span-1 w-full`}
-                >
-                  <h3 className="text-3xl font-bold text-black group-hover:text-white">
-                    {item.title}
-                  </h3>
-                  <p className="text-black group-hover:text-white">
-                    {item.subtitle}
-                  </p>
-                </motion.div>
-              ))}
-          </div>
-
-          {/* Animated DAU Logo */}
-          {/* <motion.div
-                        initial={{ opacity: 0, y: 20 }}
-                        whileInView={{ opacity: 1, y: 0 }}
-                        viewport={{ once: true, amount: 0.5 }}
-                        transition={{ duration: 0.5 }}
-                        className="hidden md:block"
+        <motion.div 
+                    initial={{ opacity: 0, y: 20 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ duration: 0.5 }}
+                    className="bg-white p-6 rounded-lg text-center mt-20"
+          >
+            <h2 className="text-2xl font-semibold mb-16">
+              Programs offered under{" "}
+              <span className="bg-gradient-to-r from-[#EF4023] to-[#FCBB4D] text-white px-4 py-1 rounded-lg">
+                M.Des
+              </span>
+            </h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                {[
+                    "Intelligent UX Design",
+                     "Communication Design",
+                ].map((specialization, index) => (
+                    <motion.div
+                        key={index}
+                        whileHover={{ scale: 1.05 }}
+                        transition={{ duration: 0.2 }}
+                        className="border-l-[1px] border-r-[1px] border-b-[1px] border-[#EF4023] p-4 rounded-lg shadow-md bg-gray-100 hover:bg-red-600 group"
                     >
-                        <img src="/DAU_Logo.png" alt="DAU Logo" />
-                    </motion.div> */}
-        </motion.div>
+                        <span className="group-hover:text-white">{specialization}</span>
+                    </motion.div>
+                ))}
+            </div>
+          </motion.div>
+
       </div>
     </section>
   );
