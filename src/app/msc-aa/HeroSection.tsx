3d01f409c91da2d9876a8429eb680541261415d4
DrakeRing2099 <sarthakpathak567@gmail.com>
2025-05-12 09:57:46 +0530

    placement changes

--- src/app/msc-aa/HeroSection.tsx
+++ src/app/msc-aa/HeroSection.tsx
@@ -143,44 +143,7 @@ const HeroSection = () => {
             </div>
           </motion.div>
 
-          <motion.div
-            className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-10 px-4 md:px-8 max-w-6xl mx-auto"
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true, amount: 0.5 }}
-            variants={{
-              visible: {
-                opacity: 1,
-                y: 0,
-                transition: { staggerChildren: 0.1, duration: 0.5 },
-              },
-              hidden: { opacity: 0, y: 20 },
-            }}
-          >
-            {/* Rectangular 5 LPA section */}
-            <motion.div
-              whileHover={{ scale: 1.05, transition: { duration: 0 } }}
-              className="px-6 py-6 w-full max-w-md h-full md:h-[50%] md:my-auto shadow-md rounded-md transition-transform duration-300 hover:shadow-lg bg-gray-100 hover:bg-red-600 group mx-auto"
-            >
-              <h3 className="text-3xl font-bold text-black group-hover:text-white">
-                5 LPA
-              </h3>
-              <p className="text-black group-hover:text-white">
-                Median Placement
-              </p>
-            </motion.div>
-
-            {/* Square image container */}
-            <div className="relative w-full max-w-md h-64 mx-auto">
-              <Image
-                src="/with placement.png"
-                alt="With Placement"
-                layout="fill"
-                objectFit="cover"
-                className="rounded-md"
-              />
-            </div>
-          </motion.div>
+         
         </div>
       </section>
     );
